b65894631c3b4b06a8059c2850678e5daf3da9ab
Mateusz Klepacki <mateusz2247@gmail.com>
2023-04-28 14:36:34 +0200

    add ne route

--- src/App.vue
+++ src/App.vue
@@ -9,17 +9,28 @@
 					to="/"></v-list-item>
 
 				<v-list-item
+					prepend-icon="mdi-trash-can"
+					title="Removed tasks"
+					value="Removed tasks"
+					to="/Removed"></v-list-item>	
+
+				<v-list-item
 					prepend-icon="mdi-forum"
 					title="About"
 					value="about"
 					to="/about"></v-list-item>
+
+				
+
+
 			</v-list>
+			
 		</v-navigation-drawer>
 		//
 		<v-app-bar
 			
 			color="teal-darken-4"
-			image="https://picsum.photos/1920/1080?random">
+			>
 			<template v-slot:image>
 				<v-img
 					gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"></v-img>
---
+++ src/views/RemovedView.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-list select-strategy="classic" class="pt-0">
+			<div v-for="task in tasks" :key="task.id">
+				<v-list-item :class="{ 'bg-green': task.done }">
+					<template v-slot:prepend>
+						<v-list-item-action>
+							<v-checkbox-btn
+								@click="doneTask(task.id)"
+								:model-value="task.done"></v-checkbox-btn>
+						</v-list-item-action>
+						<v-list-item-title
+							:class="{ 'text-decoration-line-through': task.done }"
+							>{{ task.title }}</v-list-item-title
+						>
+
+						<v-list-item-action>
+							<v-btn right
+								@click="deleteTask(task.id)"
+								color="red"
+								icon="mdi-delete"
+								variant="text"></v-btn>
+						</v-list-item-action>
+					</template>
+				</v-list-item>
+				<v-divider></v-divider>
+			</div>
+		</v-list>
+</template>
